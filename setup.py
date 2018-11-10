713269d40042a27405f008207dc52227f5ebfbb6
Ezra Peisach <ezra.peisach@rcsb.org>
2018-11-10 09:06:08 -0500

    Add dependency on rcsb.utils.multiproc

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'console_scripts': []
     },
     #
-    install_requires=['mmcif', 'scandir', 'mysqlclient', 'wwpdb.utils.config', 'wwpdb.io'],
+    install_requires=['mmcif', 'scandir', 'mysqlclient', 'wwpdb.utils.config', 'wwpdb.io', 'rcsb.utils.multiproc'],
     packages=find_packages(exclude=['wwpdb.utils.tests-db', 'examples', 'mock-data', 'tests.*']),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:
