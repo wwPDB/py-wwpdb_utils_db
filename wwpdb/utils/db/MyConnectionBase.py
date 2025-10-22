724ec366b78723c395c7c1801b2aca36b16fc9f5
Ezra Peisach <ezra.peisach@rcsb.org>
2025-10-22 12:10:55 -0400

    v0.34.1 -- correct STATUS vs MESSAGE database swapped name

--- wwpdb/utils/db/MyConnectionBase.py
+++ wwpdb/utils/db/MyConnectionBase.py
@@ -112,21 +112,20 @@ class MyConnectionBase:
             self.__dbPw = self._cI.get("SITE_DISTRO_DB_PASSWORD")
 
         elif resourceName == "STATUS":
+            self.__databaseName = self._cI.get("SITE_DB_DATABASE_NAME")
+            self.__dbHost = self._cI.get("SITE_DB_HOST_NAME")
+            self.__dbPort = self._cI.get("SITE_DB_PORT_NUMBER")
+            self.__dbSocket = self._cI.get("SITE_DB_SOCKET")
+            self.__dbUser = self._cI.get("SITE_DB_USER_NAME")
+            self.__dbPw = self._cI.get("SITE_DB_PASSWORD")
+
+        elif resourceName == "MESSAGE":
             self.__databaseName = self._cI.get("SITE_MESSAGE_DB_DATABASE_NAME")
             self.__dbHost = self._cI.get("SITE_MESSAGE_DB_HOST_NAME")
             self.__dbPort = self._cI.get("SITE_MESSAGE_DB_PORT_NUMBER")
             self.__dbSocket = self._cI.get("SITE_MESSAGE_DB_SOCKET")
             self.__dbUser = self._cI.get("SITE_MESSAGE_DB_USER_NAME")
             self.__dbPw = self._cI.get("SITE__MESSAGE_DB_PASSWORD")
-
-        elif resourceName == "MESSAGE":
-            self.__databaseName = self._cI.get("SITE_DB_DATABASE_NAME")
-            self.__dbHost = self._cI.get("SITE_DB_HOST_NAME")
-            self.__dbPort = self._cI.get("SITE_DB_PORT_NUMBER")
-            self.__dbSocket = self._cI.get("SITE_DB_SOCKET")
-
-            self.__dbUser = self._cI.get("SITE_DB_USER_NAME")
-            self.__dbPw = self._cI.get("SITE_DB_PASSWORD")
 
         else:
             pass
